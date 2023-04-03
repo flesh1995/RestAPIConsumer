26cbdeacc5e7b89c5607838646c2cb29a38296f3
work <stif2000.95@mail.ru>
2023-04-03 17:25:29 +0300

    12345

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Consumer {
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://94.198.50.185:7081/api/users";
+        String response = restTemplate.getForObject(url, String.class);
+
+        System.out.println(response);
+
+//        Map<String, String> stringMap = new HashMap<>();
+//        stringMap.put("id", "3");
+//        stringMap.put("name", "James");
+//        stringMap.put("lastName", "Brown");
+//
+//        HttpEntity<Map<String,String>> request = new HttpEntity<>(stringMap);
+//        String url1 = "http://94.198.50.185:7081/api/users";
+//        String response1 = restTemplate.postForObject(url1, request, String.class);
+//
+//
+//
+//        System.out.println(response1);
+    }
 }
